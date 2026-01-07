6aaedf937a15573e37dcba4515384af499c6f46c
Mudit Bahuguna <bahugunamk123@gmail.com>
2026-01-07 18:52:15 +0530

    express

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is working!");
+});
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Hello from Express backend!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
