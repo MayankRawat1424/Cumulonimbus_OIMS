a1c268b9097f8513cb27990ac995e895807459af
Mudit Bahuguna <bahugunamk123@gmail.com>
2026-01-09 11:51:07 +0530

    added route for supplier

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import productRoutes from "./routes/products.js";
+import supplierRoutes from "./routes/suppliers.js";
 
 const app = express();
 const PORT = 5000;
@@ -8,6 +9,7 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 app.use("/api", productRoutes);
+app.use("/api", supplierRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
