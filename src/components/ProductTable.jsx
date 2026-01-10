7810e408be1c11b6ffc66883d414cb0e39d75e49
Mayank <mayankrawat1424@gmail.com>
2026-01-10 19:48:01 +0530

    Changed Styles

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -21,27 +21,43 @@ const ProductTable = () => {
   if (loading) return <p>Loading products...</p>;
 
   return (
-    <div className="w-2xl py-4 px-6 mx-auto mt-16 border border-black shadow-lg">
+    <div className="w-4xl py-4 px-6 mx-auto mt-16 shadow-lg bg-white rounded-md">
       <h1 className="font-bold text-3xl mb-6">Products</h1>
       {products.length === 0 ? (
         <p>No product found</p>
       ) : (
-        <table>
+        <table className="w-full border border-gray-300">
           <thead>
-            <tr>
-              <th>Product</th>
-              <th>Description</th>
-              <th>Price</th>
-              <th>Quantity</th>
+            <tr className="bg-gray-300 text-gray-800 text-left">
+              <th className="px-2 py-1">Product</th>
+              <th className="px-2 py-1 max-w-20">Description</th>
+              <th className="px-2 py-1">Price</th>
+              <th className="px-2 py-1">Quantity</th>
+              <th className="px-2 py-1 w-8">Actions</th>
             </tr>
           </thead>
-          <tbody>
+          <tbody className="">
             {products.map((product) => (
-              <tr key={product.id}>
-                <td>{product.productName}</td>
-                <td>{product.productDescription}</td>
-                <td>{product.price}</td>
-                <td>{product.quantity}</td>
+              <tr key={product.id} className="border-b border-gray-300">
+                <td className="px-2 py-1">{product.productName}</td>
+                <td className="px-2 py-1 max-w-32">
+                  {product.productDescription}
+                </td>
+                <td className="px-2 py-1">{product.price}</td>
+                <td className="px-2 py-1">{product.quantity}</td>
+                <td className="px-2 py-1">
+                  <div className="flex flex-row gap-2">
+                    <button className="bg-blue-500 text-white py-1 w-20 hover:bg-blue-600 hover:cursor-pointer">
+                      Edit
+                    </button>
+                    <button
+                      className="bg-red-500 text-white py-1 w-20
+                      hover:bg-red-600 hover:cursor-pointer"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </td>
               </tr>
             ))}
           </tbody>
