42b91fdb7d2b5f8dd7a0694d25daa606577bd071
Mayank <mayankrawat1424@gmail.com>
2026-01-14 20:35:22 +0530

    added functioning product deatils

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,12 @@ const Header = ({ setWindow }) => {
     <header className="top-0 z-5 ">
       <div className="grid grid-cols-5 items-center h-16 bg-white border-b-2 border-gray-300 px-8">
         <div className="col-span-1 h-full border-r-2 border-gray-300 flex items-center">
-          <h1 className="font-semibold text-xl font-serif">Cumulonimbus .</h1>
+          <h1 className="font-semibold text-2xl font-serif">Cumulonimbus .</h1>
         </div>
         <div className="col-span-4">
           <div className="flex items-center justify-between">
             <p className="text-sm text-gray-500 pl-4">
-              Retail & logistic solutions for your business
+              Intuitive Retail & logistic solutions for your business
             </p>
             <div className="text-sm font-semibold flex items-center gap-16">
               <button
