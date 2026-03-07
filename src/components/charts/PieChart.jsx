e41b5558df0f65dce89431e93c7e1b47591f50ff
Mayank <mayankrawat1424@gmail.com>
2026-03-07 11:35:20 +0530

    Valuation Route fix

--- src/components/charts/PieChart.jsx
+++ src/components/charts/PieChart.jsx
@@ -25,11 +25,21 @@ const PieChart = ({ chartData }) => {
         data: chartData.values,
         label: "Distribution",
         backgroundColor: [
-          "rgba(255, 99, 132, 0.6)",
-          "rgba(54, 162, 235, 0.6)",
-          "rgba(255, 206, 86, 0.6)",
-          "rgba(75, 192, 192, 0.6)",
-          "rgba(153, 102, 255, 0.6)",
+          "rgba(255, 99, 132, 0.6)", // red
+          "rgba(54, 162, 235, 0.6)", // blue
+          "rgba(255, 206, 86, 0.6)", // yellow
+          "rgba(75, 192, 192, 0.6)", // teal
+          "rgba(153, 102, 255, 0.6)", // purple
+          "rgba(255, 159, 64, 0.6)", // orange
+          "rgba(199, 199, 199, 0.6)", // gray
+          "rgba(83, 102, 255, 0.6)", // indigo
+          "rgba(255, 99, 255, 0.6)", // pink
+          "rgba(99, 255, 132, 0.6)", // green
+          "rgba(255, 205, 210, 0.6)", // light pink
+          "rgba(129, 212, 250, 0.6)", // sky blue
+          "rgba(255, 241, 118, 0.6)", // light yellow
+          "rgba(174, 213, 129, 0.6)", // light green
+          "rgba(179, 157, 219, 0.6)", // lavender
         ],
       },
     ],
